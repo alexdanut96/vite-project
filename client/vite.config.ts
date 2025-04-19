72410f06d2509025866ea82d8acb262b5723d327
alexdanut96 <alexandruburcea1996@gmail.com>
2025-04-19 16:14:48 +0300

    configure project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  build: {
+    outDir: "./build",
+  },
+  resolve: {
+    alias: {
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@script": path.resolve(__dirname, "./src/script"),
+      "@media": path.resolve(__dirname, "./src/media"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+    },
+  },
+});
