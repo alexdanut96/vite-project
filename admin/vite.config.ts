38c9326581d83095a5d6096eb45e944f4c8c231a
danut <alex@servcast.net>
2025-06-12 12:58:33 +0300

    changes in vite.config.ts

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,32 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-      server: {
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const endpoint = env.VITE_API_ENDPOINT;
+
+  return {
+    plugins: [react(), svgr()],
+    server: {
       proxy: {
-        '/api': {
-          target: 'http://localhost:5000',
+        "/api": {
+          target: endpoint,
           changeOrigin: true,
           secure: false,
         },
       },
     },
-  build: {
-    outDir: "./build",
-  },
-  resolve: {
-    alias: {
-      "@routes": path.resolve(__dirname, "./src/routes"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@script": path.resolve(__dirname, "./src/script"),
-      "@media": path.resolve(__dirname, "./src/media"),
-      "@lib": path.resolve(__dirname, "./src/lib"),
-      "@globalWrappers": path.resolve(__dirname, "./src/globalWrappers"),
+    build: {
+      outDir: "./build",
     },
-  },
+    resolve: {
+      alias: {
+        "@routes": path.resolve(__dirname, "./src/routes"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+        "@script": path.resolve(__dirname, "./src/script"),
+        "@media": path.resolve(__dirname, "./src/media"),
+        "@lib": path.resolve(__dirname, "./src/lib"),
+        "@globalWrappers": path.resolve(__dirname, "./src/globalWrappers"),
+      },
+    },
+  };
 });
