cb2ba3eaa9c9314f92baa105660f3b30e8ad1a0f
alexdanut96 <alexandruburcea1996@gmail.com>
2025-06-18 20:56:06 +0300

    add new file

---
+++ admin/src/routes/Login/components/GoogleButton/index.tsx
@@ -0,0 +1,9 @@
+const google_client_id = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+// Add a type definition for the Google CredentialResponse
+
+const GoogleButton = () => {
+  return <div>asa</div>;
+};
+
+export { GoogleButton };
